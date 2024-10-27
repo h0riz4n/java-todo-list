263e31b223c04605269c64a7b1be4df80094a4fc
Rustam Khudoyan <johndoe@example.com>
2024-10-27 18:45:17 +0300

    Initial commit

---
+++ src/test/java/ru/todo/java_todo_list/JavaTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.todo.java_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
